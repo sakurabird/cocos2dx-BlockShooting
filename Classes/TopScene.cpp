bb977c09edb79a3e46a81917c4e8ea4ca13d7e4b
Yukari Sakurai <neko3genki@gmail.com>
2014-04-25 16:59:22 +0900

    トップメニューにアニメーション追加

--- Classes/TopScene.cpp
+++ Classes/TopScene.cpp
@@ -47,16 +47,43 @@ bool TopScene::init()
 
 void TopScene::makeLabel()
 {
-    CCLabelTTF* startLabel1 = CCLabelTTF::create("ゲームスタート!", "Arial", 40.0);
+    CCLabelTTF* startLabel1 = CCLabelTTF::create("ゲームスタート!", "Arial", 45.0);
     startLabel1->setColor(ccc3(255,192,203));
     CCMenuItemLabel* item1 = CCMenuItemLabel::create(startLabel1, this, menu_selector(TopScene::tapStartButton));
 
-    CCLabelTTF* startLabel2 = CCLabelTTF::create("Quit", "Arial", 40.0);
+    CCLabelTTF* startLabel2 = CCLabelTTF::create("Quit", "Arial", 45.0);
     startLabel1->setColor(ccc3(255,192,203));
     CCMenuItemLabel* item2 = CCMenuItemLabel::create(startLabel2, this, menu_selector(TopScene::menuCloseCallback));
 
     CCMenu* menu = CCMenu::create( item1, item2, NULL );
-    menu->alignItemsVerticallyWithPadding(25.0);
+    menu->alignItemsVerticallyWithPadding(30.0);
+
+    // elastic effect
+    CCSize s = CCDirector::sharedDirector()->getWinSize();
+
+    int i=0;
+    CCNode* child;
+    CCArray * pArray = menu->getChildren();
+    CCObject* pObject = NULL;
+    CCARRAY_FOREACH(pArray, pObject)
+    {
+        if(pObject == NULL)
+            break;
+
+        child = (CCNode*)pObject;
+
+        CCPoint dstPoint = child->getPosition();
+        int offset = (int) (s.width/2 + 50);
+        if( i % 2 == 0)
+            offset = -offset;
+
+        child->setPosition( ccp( dstPoint.x + offset, dstPoint.y) );
+        child->runAction(
+                         CCEaseElasticOut::create(CCMoveBy::create(2, ccp(dstPoint.x - offset,0)), 0.35f)
+                         );
+        i++;
+    }
+
 
     addChild( menu );
 }
